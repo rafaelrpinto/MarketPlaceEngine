c3aa0436cf24d7fc7e0ed509d044ffd5dab1cfcb
RAPRP <raprp@posteo.de>
2016-08-19 01:58:16 -0300

    Separated folder by client/serve

--- simplemeanapp/app.js
+++ simplemeanapp/app.js
@@ -1,63 +1,41 @@
+
+// Required libs
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-//Mongo config
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/marketplace-engine', function(err) {
-    if(err) {
-        console.log('connection error', err);
-    } else {
-        console.log('connection successful');
-    }
-});
 
+// Required files
+var indexRoute = require('./server/route/indexRoute');
+var userRoute = require('./server/route/userRoute');
+
+// Express setup
 var app = express();
-
-// view engine  
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'server/view'));
 app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'client/public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/public')));
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//Routes
+app.use('/', indexRoute);
+app.use('/user', userRoute);
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
+// 404 error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// internal error handler
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -66,8 +44,13 @@ app.use(function(err, req, res, next) {
   });
 });
 
-var server = app.listen(8080,function(){
-  console.log('express server listening on port ' + server.address().port);
+// Mongo connection setup
+mongoose.connect('mongodb://localhost/simplemeanapp', function(err) {
+    if(err) {
+        console.log('connection error', err);
+    } else {
+        console.log('connection successful');
+    }
 });
 
 
